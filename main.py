61738f15bb4f9da1d7c8102ea93863e725f9c604
Sébastien Siddi <sebastien.siddi@gmail.com>
2023-11-22 11:21:48 +0100

    dynamic tracks and steps add

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.properties import ObjectProperty
 from kivy.uix.relativelayout import RelativeLayout
+
+from track import TrackWidget
 
 Builder.load_file("track.kv")
 
+NB_TRACKS = 4
+
 
 class MainWidget(RelativeLayout):
-    pass
+    tracks_layout = ObjectProperty()
+
+    def on_parent(self, widget, parent):
+        for i in range(0, NB_TRACKS):
+            self.tracks_layout.add_widget(TrackWidget())
 
 
 class MrBeatApp(App):
