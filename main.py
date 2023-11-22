5618d1bfff784b0e246f83fdbfbee720df7ff656
Sébastien Siddi <sebastien.siddi@gmail.com>
2023-11-22 10:02:20 +0100

    application initialisation

---
+++ main.py
@@ -0,0 +1,13 @@
+from kivy.app import App
+from kivy.uix.relativelayout import RelativeLayout
+
+
+class MainWidget(RelativeLayout):
+    pass
+
+
+class MrBeatApp(App):
+    pass
+
+
+MrBeatApp().run()
