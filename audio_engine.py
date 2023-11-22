2bb8139bfda29d72ed05e500807faf9a9395051a
Sébastien Siddi <sebastien.siddi@gmail.com>
2023-11-22 17:02:18 +0100

    create audio_engine, audio_source_one_shot using audiostream

---
+++ audio_engine.py
@@ -0,0 +1,20 @@
+from audiostream import get_output
+
+from audio_source_one_shot import AudioSourceOneShot
+
+
+class AudioEngine:
+    NB_CHANNELS = 1
+    SAMPLE_RATE = 44100
+    BUFFER_SIZE = 1024
+
+    def __init__(self):
+        self.output_stream = get_output(channels=self.NB_CHANNELS,
+                                        rate=self.SAMPLE_RATE,
+                                        buffersize=self.BUFFER_SIZE)
+
+        self.audio_source_one_shot = AudioSourceOneShot(self.output_stream)
+        self.audio_source_one_shot.start()
+
+    def play_sound(self, wav_samples):
+        self.audio_source_one_shot.set_wav_samples(wav_samples)
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from kivy.lang import Builder
 from kivy.properties import ObjectProperty
 from kivy.uix.relativelayout import RelativeLayout
 
+from audio_engine import AudioEngine
 from sound_kit_service import SoundKitService
 from track import TrackWidget
 
@@ -16,10 +17,15 @@ class MainWidget(RelativeLayout):
         super(MainWidget, self).__init__(**kwargs)
         self.sound_kit_service = SoundKitService()
 
+        # kick_sound = self.sound_kit_service.get_sound_at(0)
+
+        self.audio_engine = AudioEngine()
+        # self.audio_engine.play_sound(kick_sound.samples)
+
     def on_parent(self, widget, parent):
         for i in range(0, self.sound_kit_service.get_nb_tracks()):
             sound = self.sound_kit_service.get_sound_at(i)
-            self.tracks_layout.add_widget(TrackWidget(sound))
+            self.tracks_layout.add_widget(TrackWidget(sound, self.audio_engine))
 
 
 class MrBeatApp(App):
